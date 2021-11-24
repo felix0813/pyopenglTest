51c03112fbf8e144ae29af731986cc9a1060ebb2
felix <1306332027@qq.com>
2021-11-24 21:37:02 +0800

    "增加了模型和细分与简化"

--- objLoader.py
+++ objLoader.py
@@ -1,12 +1,131 @@
 import numpy as np
 from OpenGL.GL import *
-
+import math
 from face import Face
 from vertex import Vertex
 
 
 class ObjLoader:
     buffer = []
+
+    @staticmethod
+    def load_material(file):
+        vertexes = []
+        faces = []
+        with open(file, mode='r', encoding='utf-8') as f:
+            line = f.readline()
+            while line:
+                values = line.split()
+                if values == [] or values[0] == "#" or values[0] == "vt":
+                    line = f.readline()
+                    continue
+                if values[0] == "v":
+                    vertex = Vertex(values[1], values[2], values[3])
+                    vertexes.append(vertex)
+                elif values[0] == "f":
+                    face = Face(values[1].split('/')[0], values[2].split('/')[0], values[3].split('/')[0])
+                    faces.append(face)
+                line = f.readline()
+        for face in faces:
+            glBegin(GL_TRIANGLES)
+            glVertex3f(vertexes[face.v1 - 1].x, vertexes[face.v1 - 1].y, vertexes[face.v1 - 1].z)
+            glVertex3f(vertexes[face.v2 - 1].x, vertexes[face.v2 - 1].y, vertexes[face.v2 - 1].z)
+            glVertex3f(vertexes[face.v3 - 1].x, vertexes[face.v3 - 1].y, vertexes[face.v3 - 1].z)
+            glEnd()
+
+    @staticmethod
+    def vertex_clustering_simplify(file):
+
+        vertexes = []
+        faces = []
+        with open(file, mode='r', encoding='utf-8') as f:
+            line = f.readline()
+            while line:
+                values = line.split()
+                if values == [] or values[0] == "#" or values[0] == "vt":
+                    line = f.readline()
+                    continue
+                if values[0] == "v":
+                    vertex = Vertex(values[1], values[2], values[3])
+                    vertex.x = float(values[1])
+                    vertex.y = float(values[2])
+                    vertex.z = float(values[3])
+                    '''print(vertex.x)
+                    print(vertex.y)
+                    print(vertex.z)'''
+                    vertexes.append(vertex)
+                elif values[0] == "f":
+                    face = Face(values[1].split('/')[0], values[2].split('/')[0], values[3].split('/')[0])
+                    face.v1 = int(values[1].split('/')[0])
+                    face.v2 = int(values[2].split('/')[0])
+                    face.v3 = int(values[3].split('/')[0])
+                    faces.append(face)
+                line = f.readline()
+        new_faces = []
+        new_vertex_dict = {}
+        old_to_new_dict = {}
+        new_face_set = set()
+        count = 0
+        for vertex in vertexes:
+            count = count + 1
+            x = math.floor(vertex.x)
+            y = math.floor(vertex.y)
+            z = math.floor(vertex.z)
+            new_vertex_str = str(x) + str(y) + str(z)
+            # print(str(vertex.x) + str(vertex.y) + str(vertex.z))
+            # print(new_vertex_str)
+            if new_vertex_dict.get(new_vertex_str, -1) == -1:
+                # new_faces.append(Face(x, y, z))
+                new_vertex_dict[new_vertex_str] = len(new_vertex_dict) + 1
+                old_to_new_dict[count] = len(new_vertex_dict)
+            else:
+                old_to_new_dict[count] = new_vertex_dict[new_vertex_str]
+            # print(str(count) + " " + str(old_to_new_dict[count]))
+        print(str(len(old_to_new_dict)) + " " + str(len(new_vertex_dict)))
+        for face in faces:
+            new_face = Face(old_to_new_dict[face.v1], old_to_new_dict[face.v2],
+                            old_to_new_dict[face.v3])  # to be finished
+            new_face.v1 = old_to_new_dict[face.v1]
+            new_face.v2 = old_to_new_dict[face.v2]
+            new_face.v3 = old_to_new_dict[face.v3]
+            new_face_str = str(new_face.v1) + str(new_face.v2) + str(new_face.v3)
+            if new_face_str not in new_face_set:
+                new_face_set.add(new_face_str)
+                if new_face.v1 != new_face.v2 and new_face.v1 != new_face.v3 and new_face.v2 != new_face.v3:
+                    # if not new_face.v1 == new_face.v2 == new_face.v3:
+                    new_faces.append(new_face)
+        '''for face in new_faces:
+            print(str(face.v1) + " " + str(face.v2) + " " + str(face.v3) + "\n")'''
+        print(str(len(faces)) + " " + str(len(new_faces)) + " " + str(
+            float(len(faces) - len(new_faces)) / float(len(faces))))
+
+    @staticmethod
+    def loop_subdivision(file):
+        vertexes = []
+        faces = []
+        with open(file, mode='r', encoding='utf-8') as f:
+            line = f.readline()
+            while line:
+                values = line.split()
+                if values == [] or values[0] == "#" or values[0] == "vt":
+                    line = f.readline()
+                    continue
+                if values[0] == "v":
+                    vertex = Vertex(values[1], values[2], values[3])
+                    vertex.x = float(values[1])
+                    vertex.y = float(values[2])
+                    vertex.z = float(values[3])
+                    '''print(vertex.x)
+                    print(vertex.y)
+                    print(vertex.z)'''
+                    vertexes.append(vertex)
+                elif values[0] == "f":
+                    face = Face(values[1].split('/')[0], values[2].split('/')[0], values[3].split('/')[0])
+                    face.v1 = int(values[1].split('/')[0])
+                    face.v2 = int(values[2].split('/')[0])
+                    face.v3 = int(values[3].split('/')[0])
+                    faces.append(face)
+                line = f.readline()
 
     @staticmethod
     def search_data(data_values, coordinates, skip, data_type):
@@ -72,31 +191,6 @@ class ObjLoader:
             start = i * 8
             end = start + 8
             print(buffer[start:end])'''
-
-    @staticmethod
-    def load_material(file):
-        vertexes = []
-        faces = []
-        with open(file, mode='r', encoding='utf-8') as f:
-            line = f.readline()
-            while line:
-                values = line.split()
-                if values == [] or values[0] == "#" or values[0] == "vt":
-                    line = f.readline()
-                    continue
-                if values[0] == "v":
-                    vertex = Vertex(values[1], values[2], values[3])
-                    vertexes.append(vertex)
-                elif values[0] == "f":
-                    face = Face(values[1].split('/')[0], values[2].split('/')[0], values[3].split('/')[0])
-                    faces.append(face)
-                line = f.readline()
-        for face in faces:
-            glBegin(GL_TRIANGLES)
-            glVertex3f(vertexes[face.v1 - 1].x, vertexes[face.v1 - 1].y, vertexes[face.v1 - 1].z)
-            glVertex3f(vertexes[face.v2 - 1].x, vertexes[face.v2 - 1].y, vertexes[face.v2 - 1].z)
-            glVertex3f(vertexes[face.v3 - 1].x, vertexes[face.v3 - 1].y, vertexes[face.v3 - 1].z)
-            glEnd()
 
     @staticmethod
     def load_model(file, sorted=True):
---
+++ testSimplify.py
@@ -0,0 +1,10 @@
+import objLoader
+
+model_list = ["model/test.obj", "model/人顶罐6.23.obj", "model/小型人鸟.obj", "model/族长面具1一号.obj", "model/祭司人2号.obj",
+              "model/祭司人3号.obj", "model/祭祀人1号.obj", "model/青铜树.obj", "model/面具1号.obj", "model/面具6.23.obj",
+              "model/鸡6.23.obj", "model/黄金面具6.23.obj"]
+i = 0
+for name in model_list:
+    i = i + 1
+    print(str(i)+".")
+    objLoader.ObjLoader.vertex_clustering_simplify(name)
